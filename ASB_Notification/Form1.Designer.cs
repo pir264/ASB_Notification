4ee41cedb44e85168c3b3e576c7e4c4c114f5e5b
Peter Windemuller <p.windemuller@cloudrepublic.nl>
2024-01-18 13:54:15 +0100

    Add Controls

--- ASB_Notification/Form1.Designer.cs
+++ ASB_Notification/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            notifyIcon1 = new NotifyIcon(components);
+            ASBConnectionString = new TextBox();
+            BtnSave = new Button();
+            btnTestConnetion = new Button();
+            cbDeleteMessage = new CheckBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            tbTimerInterval = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // ASBConnectionString
+            // 
+            ASBConnectionString.Location = new Point(37, 83);
+            ASBConnectionString.Multiline = true;
+            ASBConnectionString.Name = "ASBConnectionString";
+            ASBConnectionString.Size = new Size(1114, 176);
+            ASBConnectionString.TabIndex = 0;
+            // 
+            // BtnSave
+            // 
+            BtnSave.Location = new Point(48, 742);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new Size(112, 34);
+            BtnSave.TabIndex = 2;
+            BtnSave.Text = "Save";
+            BtnSave.UseVisualStyleBackColor = true;
+            BtnSave.Click += button1_Click;
+            // 
+            // btnTestConnetion
+            // 
+            btnTestConnetion.Location = new Point(42, 288);
+            btnTestConnetion.Name = "btnTestConnetion";
+            btnTestConnetion.Size = new Size(221, 34);
+            btnTestConnetion.TabIndex = 3;
+            btnTestConnetion.Text = "Test Connection";
+            btnTestConnetion.UseVisualStyleBackColor = true;
+            // 
+            // cbDeleteMessage
+            // 
+            cbDeleteMessage.AutoSize = true;
+            cbDeleteMessage.Location = new Point(41, 396);
+            cbDeleteMessage.Name = "cbDeleteMessage";
+            cbDeleteMessage.Size = new Size(163, 29);
+            cbDeleteMessage.TabIndex = 4;
+            cbDeleteMessage.Text = "Delete Message";
+            cbDeleteMessage.UseVisualStyleBackColor = true;
+            cbDeleteMessage.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 25);
+            label1.TabIndex = 5;
+            label1.Text = "ASB Connectionstring";
+            label1.Click += label1_Click;
+            // 
+            // tbTimerInterval
+            // 
+            tbTimerInterval.Location = new Point(44, 506);
+            tbTimerInterval.Name = "tbTimerInterval";
+            tbTimerInterval.Size = new Size(150, 31);
+            tbTimerInterval.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 463);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Timer interval";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1426, 909);
+            Controls.Add(label2);
+            Controls.Add(tbTimerInterval);
+            Controls.Add(label1);
+            Controls.Add(cbDeleteMessage);
+            Controls.Add(btnTestConnetion);
+            Controls.Add(BtnSave);
+            Controls.Add(ASBConnectionString);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private NotifyIcon notifyIcon1;
+        private TextBox ASBConnectionString;
+        private Button BtnSave;
+        private Button btnTestConnetion;
+        private CheckBox cbDeleteMessage;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private TextBox tbTimerInterval;
+        private Label label2;
     }
 }
--- ASB_Notification/Form1.cs
+++ ASB_Notification/Form1.cs
@@ -6,5 +6,25 @@ namespace ASB_Notification
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
